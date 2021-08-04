ad69e03cfa4c3dd5d982855214a68f8974b1d3bb
Kamran <kamranaminch@gmail.com>
2021-08-04 14:14:24 +0500

    Bloc Listner 1

--- lib/main.dart
+++ lib/main.dart
@@ -38,46 +38,73 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            SizedBox(height: 20.0,),
-            BlocBuilder<CounterCubit, CounterState>(
-              builder: (context, state) {
-                return Text(
-                  state.counterValue.toString(),
-                );
-              },
-            ),
-            SizedBox(height: 20.0,),
-            Row(
+      body: BlocConsumer<CounterCubit, CounterState>(
+        listener: (context, state) {
+          if (state.wasIncremented == true) {
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                content: Text('Value was incremented'),
+                duration: Duration(milliseconds: 400),
+              ),
+            );
+          } else if (state.wasIncremented == false) {
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                content: Text('Value was decremented'),
+                duration: Duration(milliseconds: 400),
+              ),
+            );
+          }
+        },
+        builder: (context, state) {
+          return Center(
+            child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                FloatingActionButton(
-                  onPressed: () {
-                    BlocProvider.of<CounterCubit>(context).increment();
-                  },
-                  tooltip: 'Increment',
-                  child: Icon(Icons.add),
+                Text(
+                  'You have pushed the button this many times:',
                 ),
-                SizedBox(
-                  width: 20.0,
-                ),
-                FloatingActionButton(
-                  onPressed: () {
-                    BlocProvider.of<CounterCubit>(context).decrement();
+                SizedBox(height: 20.0,),
+                BlocBuilder<CounterCubit, CounterState>(
+                  builder: (context, state) {
+                    if (state.counterValue < 0){
+                    return Text(
+                      'ITS NEGATIVE' + state.counterValue.toString(),
+                    );
+                    }else if
+                      (state.counterValue > 5){
+                      return Text('Hurrah its ' + state.counterValue.toString());
+                    }
+                    return Text(state.counterValue.toString(),);
                   },
-                  tooltip: 'Decrement',
-                  child: Icon(Icons.remove),
+                ),
+                SizedBox(height: 20.0,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    FloatingActionButton(
+                      onPressed: () {
+                        BlocProvider.of<CounterCubit>(context).increment();
+                      },
+                      tooltip: 'Increment',
+                      child: Icon(Icons.add),
+                    ),
+                    SizedBox(
+                      width: 20.0,
+                    ),
+                    FloatingActionButton(
+                      onPressed: () {
+                        BlocProvider.of<CounterCubit>(context).decrement();
+                      },
+                      tooltip: 'Decrement',
+                      child: Icon(Icons.remove),
+                    ),
+                  ],
                 ),
               ],
             ),
-          ],
-        ),
+          );
+        },
       ),
     );
   }
